ea54bb8a3aecd9e30a518d76bad7e4b570aebd67
Yaxyo <abdulhanovaozoda@gmail.com>
2025-06-30 23:13:34 +0500

    Update sql.py

--- sql.py
+++ sql.py
@@ -4,15 +4,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-db_name = os.getenv('DATABASE_NAME')
-db_user = os.getenv('DATABASE_USER')
-db_pass = os.getenv('DATABASE_PASSWORD')
-db_host = os.getenv('DATABASE_HOST')
-db_port = int(os.getenv('DATABASE_PORT'))
-
-
 def connect_psql():
+    db_name = os.getenv("DATABASE_NAME")
+    db_user = os.getenv("DATABASE_USER")
+    db_pass = os.getenv("DATABASE_PASSWORD")
+    db_host = os.getenv("DATABASE_HOST")
+    db_port = os.getenv("DATABASE_PORT")
+
     DATABASE_URL = f"postgresql://{db_user}:{db_pass}@{db_host}:{db_port}/{db_name}?sslmode=require"
+
     return psycopg2.connect(DATABASE_URL)
 
 """Yangi taom qo'shish"""
